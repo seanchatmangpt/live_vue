3c921b0d7a883ec0a4d4c011823a308ebafcb769
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 21:34:38 -0700

    test: guard e2e feature compile path

---
+++ test/ws5_contracts/e2e_compile_path_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveVue.WS5.E2ECompilePathTest do
+  use ExUnit.Case, async: true
+
+  test "e2e environment compiles colocated feature modules" do
+    source = File.read!("mix.exs")
+    assert source =~ ~s(defp elixirc_paths(:e2e), do: ["lib", "test/e2e/features"])
+  end
+end
